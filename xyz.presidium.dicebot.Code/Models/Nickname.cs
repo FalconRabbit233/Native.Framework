ea05696f6f0658944779a81978da5f890aceb8a2
FalconRabbit233 <34422044+FalconRabbit233@users.noreply.github.com>
2020-06-21 23:05:40 +0200

    测试model

---
+++ xyz.presidium.dicebot.Code/Models/Nickname.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace xyz.presidium.dicebot.Code.Models
+{
+    [Table("nickname")]
+    class Nickname
+    {
+        public int Id { get; set; }
+
+        public long FromGroup { get; set; }
+
+        public long FromQQ { get; set; }
+
+        [Column("nickname")]
+        public string NicknameValue { get; set; }
+    }
+}
